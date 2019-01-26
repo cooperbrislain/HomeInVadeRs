c2f4698baa8208d20381995e1fb17222bf97a5cf
Robert Hagan <rdhagan@gmail.com>
2019-01-25 23:06:47 -0800

    randomly spawning enemies that get destroyed when they hit the target

---
+++ VRTest/Assets/Scripts/AIController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls an player or enemy that seeks a target.
+/// </summary>
+public class AIController : MonoBehaviour
+{
+	public Transform target;
+    public Rigidbody body;
+	public float seekForce;
+
+    void Start()
+    {
+        if (body == null)
+        {
+            body = GetComponent<Rigidbody>();
+        }
+    }
+
+    void Update()
+    {
+		Vector3 dir = target.position - transform.position;
+		dir.Normalize();
+		body.AddForce(dir * seekForce);
+    }
+}
---
+++ VRTest/Assets/Scripts/RandomSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Destroys this object when it collides with an object.
+/// </summary>
+public class RandomSpawner : MonoBehaviour
+{
+	public GameObject spawnPrefab;
+	public float spawnIntervalSecs;
+	public Transform target;		// target that they seek
+	public List<Vector3> spawnPoints;
+	public int spawnCountPerWave = 5;
+
+	public int wave;
+
+	private float _lastSpawnTime;
+
+	void Update() {
+		if (_lastSpawnTime + spawnIntervalSecs < Time.time) {
+			int slot = Random.Range(0, spawnPoints.Count);
+			Vector3 pos = spawnPoints[slot];
+			GameObject spawn = Instantiate(spawnPrefab, pos, Quaternion.identity);
+			var ai = spawn.GetComponent<AIController>();
+			ai.target = target;
+			_lastSpawnTime = Time.time;
+		}
+	}
+}
