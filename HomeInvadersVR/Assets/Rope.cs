4459a37e36e9388acc2994e1d98bc3d3904ab0ef
Cooper Brislain <cbrislain@gmail.com>
2019-01-26 13:12:06 -0800

    added rope stuff

---
+++ HomeInvadersVR/Assets/Rope.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+
+	public float ropeLength;
+
+	public GameObject ropeLink;
+	// Use this for initialization
+	private GameObject prevLink;
+	private GameObject newLink;
+	
+	void Start () {
+		for (int i = 0; i < ropeLength*100; i+=10)
+		{
+			newLink = Instantiate(ropeLink, new Vector3(0,i*0.01f,0), Quaternion.identity, gameObject.transform);
+			if (i != 0)
+			{
+				
+				HingeJoint hinge = newLink.GetComponent(typeof(HingeJoint)) as HingeJoint;
+				hinge.connectedBody = prevLink.GetComponent<Rigidbody>();
+			}
+			prevLink = newLink;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
