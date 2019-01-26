8c491ba1888fa254c67c7ca335c33e59506b50a3
Jack Kern <jack.w.kern@gmail.com>
2019-01-26 15:57:44 -0800

    Added more enemy agent logic

--- HomeInvadersVR/Assets/Scripts/EnemyAgent.cs
+++ HomeInvadersVR/Assets/Scripts/EnemyAgent.cs
@@ -5,9 +5,11 @@ using UnityEngine.AI;
 
 public class EnemyAgent : MonoBehaviour {
 
-    public Valuable  goal;
+    public Goal      goal;
     public string    agentName;
     NavMeshAgent     agent;
+
+    private Valuable carriedItem;
 
 	// Use this for initialization
 	void Start () {
@@ -17,7 +19,40 @@ public class EnemyAgent : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (agent != null && goal != null)
-            agent.SetDestination(goal.transform.position);
+        if (goal)
+        {
+            if (agent != null)
+                agent.SetDestination(goal.transform.position);
+
+            // If we are close enough to the object to pick it up (Only on the X/Z plane)
+            Vector3 diff = (transform.position - goal.transform.position);
+            diff.y = 0;
+            float distanceToGoal2D = diff.magnitude;
+            if (distanceToGoal2D < goal.activateDistance)
+            {
+                if (goal.IsValuable())
+                {
+                    carriedItem = (Valuable)goal;
+
+                    Debug.Log(agentName + " picked up " + carriedItem.valuableName);
+                    // Parent the object under the agent (in front of it) and make it kinematic
+                    goal.transform.position = transform.position + transform.forward * 0.5f + transform.up;
+                    goal.transform.parent = transform;
+                    goal.GetComponent<Rigidbody>().isKinematic = true;
+                    
+                    goal = AgentReturnGoal.GetInstance();
+                }
+                else
+                {
+                    Debug.Log(agentName + " is throwing " + carriedItem.valuableName + " into the truck!");
+                    goal = null;
+                    carriedItem.transform.parent = null;
+                    Rigidbody valuableRB = carriedItem.GetComponent<Rigidbody>();
+                    valuableRB.isKinematic = false;
+                    valuableRB.AddForceAtPosition(transform.forward * 100, carriedItem.transform.position, ForceMode.Force);
+                }
+
+            }
+        }
 	}
 }
