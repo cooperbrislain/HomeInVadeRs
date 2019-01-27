904fc3898fb5abdcc8e812070ee0c44ab8c08b69
Jack Kern <jack.w.kern@gmail.com>
2019-01-26 23:19:59 -0800

    Ground Placement

---
+++ HomeInvadersVR/Assets/Scripts/GroundTrapPickup.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class GroundTrapPickup : VRTK_InteractableObject {
+    public static GroundTrapPickup ActivePickup = null;
+
+    public  GameObject GroundTrapPrefab;
+    [System.NonSerialized]
+    public  GameObject GroundPlacementObject;
+
+    private bool _placementValid;
+
+    // Use this for initialization
+    void Start()
+    {
+        GroundPlacementObject = Instantiate(GroundTrapPrefab);
+        GroundPlacementObject.GetComponent<BoxCollider>().enabled = false; // Disable the placement collider
+        TogglePlacementVisible(false);
+    }
+
+    public void TogglePlacementVisible(bool visible)
+    {
+        MeshRenderer[] meshRenderers = GroundPlacementObject.GetComponentsInChildren<MeshRenderer>(true);
+        foreach (MeshRenderer renderer in meshRenderers)
+            renderer.enabled = visible;
+    }
+    public void PlacePreviewAt(RaycastHit hit)
+    {
+        if (GroundPlacementObject != null)
+        {
+            GroundPlacementObject.transform.position = hit.point;
+            _placementValid = true;
+        }
+    }
+    public void DisablePlacement()
+    {
+        _placementValid = false;
+    }
+
+    public override void StartUsing(VRTK_InteractUse usingObject)
+    {
+        base.StartUsing(usingObject);
+
+        GetComponentInChildren<BoxCollider>().enabled = false;
+        usingObject.GetComponent<VRTK_Pointer>().Toggle(true);
+        ActivePickup = this;
+    }
+
+    public override void StopUsing(VRTK_InteractUse usingObject)
+    {
+        GetComponentInChildren<BoxCollider>().enabled = true;
+        usingObject.GetComponent<VRTK_Pointer>().Toggle(false);
+        ActivePickup = null;
+
+        if (_placementValid)
+            SpawnTrap();
+
+        base.StopUsing(usingObject);
+    }
+
+    void SpawnTrap()
+    {
+        GameObject newTrap = Instantiate(GroundTrapPrefab);
+        newTrap.transform.position = GroundPlacementObject.transform.position;
+    }
+
+    // Update is called once per frame
+    protected override void Update () {
+        base.Update();
+	}
+}
---
+++ HomeInvadersVR/Assets/Scripts/TrapPlacementPlane.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class TrapPlacementPlane : VRTK_InteractableObject {
+
+    VRTK_BasePointerRenderer   activePointerRenderer = null;
+
+    public override void StartUsing(VRTK_InteractUse usingObject)
+    {
+        base.StartUsing(usingObject);
+
+        // Need to make sure we are tracking start/stop for each controller? (or only allow for one controller)
+        activePointerRenderer = usingObject.GetComponent<VRTK_BasePointerRenderer>();
+        
+        usingObject.GetComponent<VRTK_Pointer>().targetListPolicy = GetComponent<VRTK_PolicyList>();
+
+        if (GroundTrapPickup.ActivePickup != null)
+            GroundTrapPickup.ActivePickup.TogglePlacementVisible(true);
+    }
+
+    public override void StopUsing(VRTK_InteractUse usingObject)
+    {
+        base.StopUsing(usingObject);
+
+        usingObject.GetComponent<VRTK_Pointer>().targetListPolicy = null;
+
+        if (GroundTrapPickup.ActivePickup != null)
+            GroundTrapPickup.ActivePickup.TogglePlacementVisible(false);
+
+        activePointerRenderer = null;
+    }
+
+    protected void Start()
+    {
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (activePointerRenderer != null && activePointerRenderer.IsTracerVisible())
+        {
+            RaycastHit hit = activePointerRenderer.GetDestinationHit();
+
+            if(hit. GroundTrapPickup.ActivePickup != null)
+                GroundTrapPickup.ActivePickup.PlacePreviewAt(hit);
+        }
+        else if (GroundTrapPickup.ActivePickup != null)
+            GroundTrapPickup.ActivePickup.DisablePlacement();
+    }
+}
