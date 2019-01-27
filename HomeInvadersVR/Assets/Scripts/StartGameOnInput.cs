ce8854948612ad8e22a1d5e77fcf4f96ec9b1243
Robert Hagan <rdhagan@gmail.com>
2019-01-27 11:50:10 -0800

    add initial StartScreen

---
+++ HomeInvadersVR/Assets/Scripts/StartGameOnInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+/// <summary>a
+/// Loads game after any input
+/// </summary>
+public class StartGameOnInput : MonoBehaviour {
+	public string sceneToLoad = "TeleportOnNavMesh";
+
+	void Update() {
+		if (Input.anyKeyDown) {
+			SceneManager.LoadScene(sceneToLoad);
+		}
+	}
+}
