2a49e2c75807516eec03dd85fbdeb9c79478810b
Jack Kern <jack.w.kern@gmail.com>
2019-01-27 01:21:59 -0800

    Added ShowOnPlay so we could hide the ceiling in edit mode

---
+++ HomeInvadersVR/Assets/Scripts/ShowOnPlay.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowOnPlay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponentInChildren<MeshRenderer>().enabled = true;
+	}
+	
+}
